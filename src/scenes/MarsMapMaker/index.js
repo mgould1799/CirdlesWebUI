8dc0159602973b6044af2b6946560895ba3d3a58
Meagan Gould <gouldma@g.cofc.edu>
2019-05-18 21:01:17 -0400

    added changes that don't work

--- src/scenes/MarsMapMaker/index.js
+++ src/scenes/MarsMapMaker/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './src/App';
 import * as serviceWorker from './src/serviceWorker';
+import React, { Component } from 'react';
+import Radium from 'radium';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-import MapBuilder from './src/components/MapBuilder'
-import XMapBuilder from './src/components/XMapBuilder'
+//import MapBuilder from './src/components/MapBuilder'
+//import XMapBuilder from './src/components/XMapBuilder'
 
 ReactDOM.render(<XMapBuilder/>, document.getElementById('root'));
 
